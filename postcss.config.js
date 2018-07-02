8f12c0e399b69bba62167efeeeceee1e0a178b95
Jens Hedqvist <jens.hedqvist@gmail.com>
2018-07-02 14:06:56 +0200

    Dotfiles for babel, IDE and postcss

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = (ctx) => ({
+  map: ctx.options.map,
+  plugins: [
+    require('postcss-import')(),
+    require('postcss-custom-properties')(),
+    require('postcss-url')([
+      {filter: '**/*.woff', url: 'inline'},
+      { url: 'copy', useHash: true }
+    ]),
+    require('postcss-flexbugs-fixes')(),
+    require('autoprefixer')({browsers: [ 'last 2 versions', 'ie >= 9', 'Firefox ESR' ]}),
+    require('postcss-clean')()
+  ]
+})
