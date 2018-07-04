b906fc825f718a52cdebf028d3ce5a91418c06d6
Jens Hedqvist <jens.hedqvist@gmail.com>
2018-07-04 20:46:11 +0200

    App unstable 0.5.1

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# astridlindgren.se
-Head app for astridlindgren.se
+# Astrid Lindgren Company
+Website for Astrid Lindgren Company that promotes and protects the Astrid Lindgren trademark all over the world.
+
+## Setup
+
+TBD - under development
+
+### Environment variables
+
+TBD - under development
+
+## Technologies
+
+This no fuzz stack is just a Node server using [Koa](http://koajs.com/) to deliver content from the headless CMS [Prismic.io](https://prismic.io/) through simple [nanohtml](https://github.com/choojs/nanohtml) views.
+
+### Koa
+TBD - under development
+### Nanohtml
+TBD - under development
+### SUIT CSS
+TBD - under development
+### Prismic
+TBD - under development
+
+### Manifest(ish)
+
+A few words from the coders of this website:
+
+- *As vanilla as possible* - we think coding for the web should be fun and hands on, embracing modern web standards helps you avoid overhead and hiding complexity under layers of frameworks.
+- *Minimalism is simplicity* - minimalism is the art of identifying and using only what you need. It may not always be easy. But we think this keeps things simple.
+- *Patterns over frameworks* - a great framework is a great framework. React is one of those. But we are curious about the innovation that happens outside the norm. We like to support the initiatives that distills the greatness of the framework giants into something that is more hands on.
+
+## Build and deploy
+TBD - under development
+
+## Special thanks
+Special thanks to codeandconspire who built the open sourced globalgoals.org website for inspiration 🤜🏼❤️🤛🏼.
+
+## License
+TBD - under development
---
+++ index.js
@@ -0,0 +1,113 @@
+var Koa = require('koa')
+var body = require('koa-body')
+var serve = require('koa-static')
+var helmet = require('koa-helmet')
+var noTrailingSlash = require('koa-no-trailing-slash')
+//var app = require('./lib/app')
+var router = require('./lib/router')
+var assets = require('./lib/middleware/assets')
+var stores = require('./lib/middleware/stores')
+var prismic = require('./lib/middleware/prismic')
+
+var server = new Koa()
+
+
+/**
+ * Compile and serve assets on demand during development
+ */
+
+if (process.env.NODE_ENV === 'development') {
+  // Serve live client resources
+  //server.use(require('./lib/middleware/watchify'))
+  server.use(require('./lib/middleware/postcss'))
+
+  // Serve components assets from disk
+  server.use(serve('lib'))
+}
+
+/**
+ * Take extra care to clean up em' headers in production
+ */
+
+if (process.env.NODE_ENV !== 'development') {
+  server.use(helmet())
+}
+
+/**
+ * Prevent indexing everything but production
+ */
+
+if (process.env.NODE_ENV !== 'production') {
+  server.use(require('./lib/middleware/robots'))
+}
+
+/**
+ * Capture special routes before any other middleware
+ */
+
+//server.use(api)
+//server.use(redirects)
+
+/**
+ * Remove trailing slashes before continuing
+ */
+
+server.use(noTrailingSlash())
+
+/**
+ * Serve static files
+ */
+
+server.use(assets)
+server.use(serve('public', { maxage: 1000 * 60 * 60 * 24 * 365 }))
+
+/**
+ * Add on Universal Analytics for server process tracking
+ */
+
+//server.use(analytics(process.env.GOOGLE_ANALYTICS_ID))
+
+/**
+ * Set up request cache mechanism
+ */
+
+//server.use(cache)
+
+/**
+ * Parse request body
+ */
+
+server.use(body())
+
+/**
+ * Set default stores
+ */
+
+server.use(stores)
+
+//
+/**
+ * Handle rendering response
+ */
+
+//server.use(render())
+
+/**
+ * Hook up the Prismic api
+ */
+
+server.use(prismic)
+
+/**
+ * Hook up em' routes
+ */
+
+server.use(router)
+
+/**
+ * Lift off
+ */
+
+server.listen(process.env.PORT, () => {
+  console.info(`🚀  Server listening at localhost:${process.env.PORT}`)
+})
