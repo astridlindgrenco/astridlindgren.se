4b5a321a7455d0237788f5a99404c0cae50e27ee
Jens Hedqvist <jens.hedqvist@gmail.com>
2018-08-10 15:50:07 +0200

    Improved client bundle structure

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ module.exports = (ctx) => ({
     require('postcss-custom-properties')(),
     require('postcss-custom-media')(),
     require('postcss-selector-matches')(),
+    require('postcss-object-fit-images')(),
     require('postcss-url')([
       {filter: '**/*.woff', url: 'inline'},
       { url: 'copy', useHash: true }
