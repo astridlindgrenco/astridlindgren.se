5e870c7dee93a472432910aa006ef9376c6aed09
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2018-08-28 14:02:18 +0200

    Handle inf subpaths for page. Add time to console.log

--- index.js
+++ index.js
@@ -124,5 +124,19 @@ server.use(router)
  */
 
 server.listen(process.env.PORT, () => {
-  console.info(`🚀  Server listening at localhost:${process.env.PORT}`)
+  // Add time to console.log
+  // FIXME: logging framework?
+  (function () {
+    if (console && console.log) {
+      let time = new Date()
+
+      var old = console.log
+      console.log = function () {
+        Array.prototype.unshift.call(arguments, '[' + time.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) + ']')
+        old.apply(this, arguments)
+      }
+    }
+  })()
+
+  console.log(`[Server] 🚀 Listening on localhost:${process.env.PORT}`)
 })
