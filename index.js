08a00c6df37ece05ec20654e993ab5ba530cc9a9
Oskar Duveborn <weyland@flowersfx.com>
2021-02-16 10:21:34 +0100

    fixed wrong csp updated

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ if (process.env.NODE_ENV === 'development') {
 server.use(helmet.contentSecurityPolicy({
   directives:{
     defaultSrc:["'self'", 'astridlindgren.prismic.io', 'astridlindgren.cdn.prismic.io', 'vars.hotjar.com'],
-    scriptSrc:["'self'", "'unsafe-inline'", "'unsafe-eval'", 'addsearch.com/', '*.hotjar.com/', 'static.cdn.prismic.io/', 'cdnjs.cloudflare.com/ajax/libs/cookieconsent2/'],
-    styleSrc:["'self'","'unsafe-inline'", 'cdnjs.cloudflare.com/ajax/libs/cookieconsent2/', 'hello.myfonts.net/count/', 'www.googletagmanager.com', 'cdn.polyfill.io'],
+    scriptSrc:["'self'", "'unsafe-inline'", "'unsafe-eval'", 'addsearch.com/', '*.hotjar.com/', 'static.cdn.prismic.io/', 'cdnjs.cloudflare.com/ajax/libs/cookieconsent2/', 'www.googletagmanager.com', 'cdn.polyfill.io'],
+    styleSrc:["'self'","'unsafe-inline'", 'cdnjs.cloudflare.com/ajax/libs/cookieconsent2/', 'hello.myfonts.net/count/'],
     imgSrc:["'self'", 'data:', 'images.prismic.io', 'astridlindgren.cdn.prismic.io'],
     connectSrc:['in.hotjar.com', 'astridlindgren.prismic.io'],
     fontSrc:["'self'", 'data:', ]}}));
